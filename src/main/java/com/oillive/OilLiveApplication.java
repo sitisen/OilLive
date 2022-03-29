5cb14500e38d690528e766501f77b36b2923841f
KimCG0 <aksuue824@gmail.com>
2022-03-29 19:31:52 +0900

    프로젝트:초기 프로젝트 업로드

---
+++ src/main/java/com/oillive/OilLiveApplication.java
@@ -0,0 +1,13 @@
+package com.oillive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilLiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oillive/OilLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oillive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
