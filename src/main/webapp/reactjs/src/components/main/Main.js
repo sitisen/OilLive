6cb242e679370b2017a11df23f593aa98f34cf0a
KimCG0 <aksuue824@gmail.com>
2022-04-07 19:57:43 +0900

    기능추가:Back-End단과 결과값 통신 테스트 코드 작성

--- src/main/webapp/reactjs/src/components/main/Main.js
+++ src/main/webapp/reactjs/src/components/main/Main.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAvgSidoPrice } from 'services/ApiService';
 
 import './Main.css';
 
 const Main = () => {
+
+  const [avgSidoPrice, setAvgSidoPrice ] = useState();
+
+  useEffect( () => {
+    getAvgSidoPrice().then( (res) => setAvgSidoPrice(res.data));
+  }, []);
+
     return (
-        <div className='main-layout'>
-          Main
+      <div className='main-layout'> 
+
+        <div className='main-img'>
+          <div className='container text-center imgtest'>
+            t
+          </div>
         </div>
+
+        <div className='container text-center test-main'>
+          <div className='box-button'>
+            유가 종류
+          </div>
+          <div className='box'>
+            지도
+            {/* <ul>
+              {avgSidoPrice && avgSidoPrice.map((data) => (
+                <li key={data.sidocd}>
+                  price : {data.price}
+                </li>
+              ))}
+            </ul> */}
+          </div>
+          <div className='box'>
+            그래프
+          </div>
+        </div>
+
+      </div>
     );
 };
 
