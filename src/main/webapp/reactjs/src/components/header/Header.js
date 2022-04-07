beaeeb7ac52e3a0d9c5d168f9a069c3eed3bc0f9
KimCG0 <aksuue824@gmail.com>
2022-04-07 19:51:10 +0900

    기능변경: 레이아웃 구조 수정

--- src/main/webapp/reactjs/src/components/header/Header.js
+++ src/main/webapp/reactjs/src/components/header/Header.js
@@ -7,18 +7,25 @@ import './Header.css';
 const Header = () => {
   return (
     <header>
+      <div className='container'>
         <nav>
             <table className='text-center'>
+              <tbody>
                 <tr>
-                    <td className='header_Nav_Img'><img alt='logo' src='/images/logo.jpg' width='250' height='100%'/></td>
+                    <td className='header_Nav_Img'><img alt='logo' src='/images/logo.jpg' width='230' height='100%'/></td>
                     <td className='header_Nav'><span className="nav_Span">차량용품</span></td>
                     <td className='header_Nav'><span className="nav_Span">이용안내</span></td>
                     <td className='header_Nav'><span className="nav_Span">전기차 충전소</span></td>
                     <td className='header_Nav'><span className="nav_Span">이벤트</span></td>
-                    <td className='header_Nav_Button'><button type="button" class="btn btn-outline-success">로그인</button></td>
+                    <td className='header_Nav_Button'>
+                      <span className='stst'>rlacjfrl</span>&nbsp;님 환영합니다.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      <button type="button" className="btn btn-outline-success">로그아웃</button>
+                    </td>
                 </tr>
+                </tbody>
             </table>
         </nav>
+      </div>
     </header>
   );
 }
