b11f3e3bb217cc6c024423f4d845b2fa221c810d
sitisen <sitisen@naver.com>
2022-04-08 18:30:11 +0900

    기능추가:로그인 페이지 라우터 기능추가

--- src/main/webapp/reactjs/src/App.js
+++ src/main/webapp/reactjs/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 // Import pages
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 // Page Routing Setting
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Navigate replace to="/users/home"/>} />
       <Route path='/users/home' element={<Home />} />
+      <Route path='/users/login' element={<Login />} />
     </Routes>
   );
 }
