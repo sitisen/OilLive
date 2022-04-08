e7e5eb02f121f832b0431289372c21b32b96f535
sitisen <sitisen@naver.com>
2022-04-08 18:23:26 +0900

    기능추가:레이아웃 완성

--- src/main/webapp/reactjs/src/components/sidebar/Sidebar.js
+++ src/main/webapp/reactjs/src/components/sidebar/Sidebar.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import './Sidebar.css';
 
 const Sidebar = () => {
+
+    const moveToTop = () => (document.documentElement.scrollTop = 0);
+
     return (
-        <div>
-            Sidebar
+        <div className='sideBarPosition'>
+            <table>
+                <thead>
+                    <tr>
+                        <td className='sideBarTdHead'><span className='sideBarText'>자주찾는메뉴</span></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='sideBarTd'>
+                            <div className='myPageImgWap'>
+                                <img src ="/images/mypage.png" width="35" height="35"/>
+                                <img src ="/images/mypageHover.png" width="35" height="35"/>
+                            </div>
+                            <span className='sideBarText'>마이페이지</span>
+                        </td>    
+                    </tr>
+                    <tr>
+                        <td className='sideBarTd'> 
+                            <div className='basketImgWap'>
+                                <img src ="/images/basket.png" width="35" height="35"/>
+                                <img src ="/images/basketHover.png" width="35" height="35"/>
+                            </div> 
+                            <span className='sideBarText'>장바구니</span>
+                        </td>    
+                    </tr>
+                    <tr>
+                        <td className='sideBarTd'>
+                            <div className='qnaImgWap'>
+                                <img src ="/images/qna.png" width="35" height="35"/>
+                                <img src ="/images/qnaHover.png" width="35" height="35"/>
+                            </div>
+                            <span className='sideBarText'>문의하기</span>
+                        </td>    
+                    </tr>
+                    <tr>
+                        <td className='sideBarTd'>
+                        <button type="button" class="topButton" onClick={moveToTop}>TOP ∧</button>
+                        </td>    
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
