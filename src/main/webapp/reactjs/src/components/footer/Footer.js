21b9edb224d7238ae0a341c77dbcd14ccd2081dd
sitisen <sitisen@naver.com>
2022-04-08 18:22:06 +0900

    기능변경:레이아웃 구조 수정

--- src/main/webapp/reactjs/src/components/footer/Footer.js
+++ src/main/webapp/reactjs/src/components/footer/Footer.js
@@ -10,7 +10,8 @@ const Footer = () => {
                 <h6><b>OilLive Inc. 서울특별시 강서구 12동 34로</b></h6>
                 <span>대표 : 홍길동</span>
                 <span>사업자 등록 번호 : 001-00-123456</span>
-                <h6><b>Copyright © 2022 OilLive.co.,Ltd. All rights reserved.</b></h6>
+                <br></br>
+                <span><b>Copyright © {new Date().getFullYear()} OilLive.co.,Ltd. All rights reserved.</b></span>
             </div>
         </footer>
     );
