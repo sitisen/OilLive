8a5fb4c221cbb3a8d3d0005d3a0c18404d163880
KimCG0 <aksuue824@gmail.com>
2022-04-11 20:08:32 +0900

    형식:코드 형식 변경

--- src/main/webapp/reactjs/src/components/header/Header.js
+++ src/main/webapp/reactjs/src/components/header/Header.js
@@ -12,14 +12,14 @@ const Header = () => {
             <table className='text-center'>
               <tbody>
                 <tr>
-                    <td className='header_Nav_Img'><img alt='logo' src='/images/logo.jpg' width='230' height='100%'/></td>
-                    <td className='header_Nav'><span className="nav_Span">차량용품</span></td>
-                    <td className='header_Nav'><span className="nav_Span">이용안내</span></td>
-                    <td className='header_Nav'><span className="nav_Span">전기차 충전소</span></td>
-                    <td className='header_Nav'><span className="nav_Span">이벤트</span></td>
-                    <td className='header_Nav_Button'>
+                    <td className='header-nav-img'><img alt='logo' src='/images/logo.jpg' width='230' height='100%'/></td>
+                    <td className='header-nav'><span className='nav-span'>차량용품</span></td>
+                    <td className='header-nav'><span className='nav-span'>이용안내</span></td>
+                    <td className='header-nav'><span className='nav-span'>전기차 충전소</span></td>
+                    <td className='header-nav'><span className='nav-span'>이벤트</span></td>
+                    <td className='header-nav-button'>
                       <span className='stst'>rlacjfrl</span>&nbsp;님 환영합니다.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                      <button type="button" className="btn btn-outline-success">로그아웃</button>
+                      <button type='button' className='btn btn-outline-success'>로그아웃</button>
                     </td>
                 </tr>
                 </tbody>
--- src/main/webapp/reactjs/src/components/sidebar/Sidebar.js
+++ src/main/webapp/reactjs/src/components/sidebar/Sidebar.js
@@ -6,44 +6,44 @@ const Sidebar = () => {
     const moveToTop = () => (document.documentElement.scrollTop = 0);
 
     return (
-        <div className='sideBarPosition'>
+        <div className='sidebar-layout'>
             <table>
                 <thead>
                     <tr>
-                        <td className='sideBarTdHead'><span className='sideBarText'>자주찾는메뉴</span></td>
+                        <td className='sidebar-thead'><span className='sidebar-td-span'>자주찾는메뉴</span></td>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td className='sideBarTd'>
-                            <div className='myPageImgWap'>
-                                <img src ="/images/mypage.png" width="35" height="35"/>
-                                <img src ="/images/mypageHover.png" width="35" height="35"/>
+                        <td className='sidebar-td'>
+                            <div className='myPage-img-wap'>
+                                <img alt='mypage' src='/images/mypage.png' width='35' height='35'/>
+                                <img alt='mypage-hover' src='/images/mypageHover.png' width='35' height='35'/>
                             </div>
-                            <span className='sideBarText'>마이페이지</span>
+                            <span className='sidebar-td-span'>마이페이지</span>
                         </td>    
                     </tr>
                     <tr>
-                        <td className='sideBarTd'> 
-                            <div className='basketImgWap'>
-                                <img src ="/images/basket.png" width="35" height="35"/>
-                                <img src ="/images/basketHover.png" width="35" height="35"/>
+                        <td className='sidebar-td'> 
+                            <div className='basket-img-wap'>
+                                <img alt='basket' src='/images/basket.png' width='35' height='35'/>
+                                <img alt='basket-hover'src='/images/basketHover.png' width='35' height='35'/>
                             </div> 
-                            <span className='sideBarText'>장바구니</span>
+                            <span className='sidebar-td-span'>장바구니</span>
                         </td>    
                     </tr>
                     <tr>
-                        <td className='sideBarTd'>
-                            <div className='qnaImgWap'>
-                                <img src ="/images/qna.png" width="35" height="35"/>
-                                <img src ="/images/qnaHover.png" width="35" height="35"/>
+                        <td className='sidebar-td'>
+                            <div className='qna-img-wap'>
+                                <img alt='qna' src='/images/qna.png' width='35' height='35'/>
+                                <img alt='qna-hover' src='/images/qnaHover.png' width='35' height='35'/>
                             </div>
-                            <span className='sideBarText'>문의하기</span>
+                            <span className='sidebar-td-span'>문의하기</span>
                         </td>    
                     </tr>
                     <tr>
-                        <td className='sideBarTd'>
-                        <button type="button" class="topButton" onClick={moveToTop}>TOP ∧</button>
+                        <td className='sidebar-td'>
+                        <button type='button' className='top-button' onClick={moveToTop}>TOP ∧</button>
                         </td>    
                     </tr>
                 </tbody>
