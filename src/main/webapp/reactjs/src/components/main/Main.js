0cf99d5fce07a04eb29f7ecba101f26701991c77
KimCG0 <aksuue824@gmail.com>
2022-04-14 21:46:18 +0900

    기능추가:시도별 평균 및 전국 평균 기능 하드코딩 -> API 값 조회로 변경

--- src/main/webapp/reactjs/src/components/main/Main.js
+++ src/main/webapp/reactjs/src/components/main/Main.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { getAvgSidoPrice } from 'services/ApiService';
+import ApiService from 'services/ApiService';
 
 // import CSS
 import mainStyle from './Main.module.css';
@@ -12,37 +12,77 @@ function now() {
 /* 메인페이지 렌더링 */
 const Main = () => {
 
-  /* Back-End 연동 */
-  // const [ avgSidoPrice, setAvgSidoPrice ] = useState();
+  /* 기름 종류 코드 */
+  const gasoline = 'B027';
+  const diesel = 'D047';
+  const lpg = 'K015';
 
-  // useEffect( () => {
-  //   getAvgSidoPrice().then( (res) => setAvgSidoPrice(res.data) );
-  // }, []);
+  /* 유가 API 요청 값 */
+  const [inputs, setInputs] = useState({
+    prodcd : gasoline,
+    sido: '',
+    sigun: ''
+  });
+
+  const {prodcd, sido, sigun} = inputs;
+
+  /* 첫 화면 렌더링 */
+  const [ oilPriceList, setOilPriceList ] = useState({});
+
+  useEffect( () => {
+
+    /* 시도별 주유소 평균가격(전국 포함) API */
+    ApiService.oilPriceList(inputs).then( (res) => setOilPriceList(res.data) );
+
+  }, [inputs]);
 
   /* 기름 종류 라디오 버튼 클릭 이벤트 */
-  const [ isChecked, setIsChecked ] = useState('0');
+  const [ isChecked, setIsChecked ] = useState(gasoline);
 
   const oilTypeCheck = (e) => {
-
+   
     let lastCount = e.currentTarget.children.length - 1;
     let child = e.currentTarget.children[lastCount];
 
-    if ( child.value === 'B027' ) {
-      setIsChecked('0');
-    } else if ( child.value === 'D047' ) {
-      setIsChecked('1');
+    if ( child.value === gasoline ) {
+
+      setIsChecked(gasoline);
+      setInputs({ ...inputs, prodcd: gasoline } );
+
+    } else if ( child.value === diesel ) {
+
+      setIsChecked(diesel);
+      setInputs({ ...inputs, prodcd: diesel });
+
     } else {
-      setIsChecked('2');
+
+      setIsChecked(lpg);
+      setInputs({ ...inputs, prodcd: lpg });
+      
     }
-
-    child.checked = true;
-
-    const prodcd = prodcd;
-
-    /* API 관련 */
-    getAvgSidoPrice(e);
     
   }
+
+  // 시도별 평균 className 부여 을 위한 객체 선언
+  const [sidoName] = useState({
+    서울 : 'seoul',
+    경기 : 'gyeonggi',
+    강원 : 'gangwon',
+    충북 : 'chungbuk',
+    충남 : 'chungnam',
+    전북 : 'jeonbuk',
+    전남 : 'jeonnam',
+    경북 : 'gyeonbuk',
+    경남 : 'gyeonnam',
+    부산 : 'busan',
+    제주 : 'jeju',
+    대구 : 'daegu',
+    인천 : 'incheon',
+    광주 : 'gwangju',
+    대전 : 'daejeon',
+    울산 : 'ulsan',
+    세종 : 'sejong'
+  });
 
   return (
     <div className={mainStyle['main-layout']}> 
@@ -54,10 +94,34 @@ const Main = () => {
               <span>전국 평균</span>
               <span>(원/리터)</span>
             </div>
-            <div>
-              <span>1980.14</span>
-              <span className={mainStyle['up-price']}>&#9650; 1.54</span>
-            </div>
+              { oilPriceList['AllList'] && oilPriceList['AllList'].filter(idx => idx.prodcd === isChecked).map( (list) => {
+
+                if(list.diff.includes('+')) { // 전일대비 전국 평균 가격이 증가했을 경우
+                  return ( 
+                    <div key={list.prodcd === isChecked}>
+                      <span>{list.price}</span>
+                      <span className={mainStyle['up-price']}>&#9650; {list.diff}</span> 
+                    </div>
+                    )
+
+                } else if(list.diff.includes('-')) { // 전일대비 전국 평균 가격이 감소했을 경우
+                  return ( 
+                    <div key={list.prodcd === isChecked}>
+                      <span>{list.price}</span>
+                      <span className={mainStyle['down-price']}>&#9660; {list.diff}</span> 
+                    </div>
+                    )
+
+                } else { // 전일대비 전국 평균 가격이 변함없을 경우
+                  return ( 
+                    <div key={list.prodcd === isChecked}>
+                      <span>{list.price}</span>
+                      <span className={mainStyle['unchange-price']}>&#45; {list.diff}</span> 
+                    </div>
+                    )                  
+                }
+
+              })}
           </div>
           <div className={mainStyle['local']}>
             <div>
@@ -82,26 +146,27 @@ const Main = () => {
           </div>
 
           <form>
-            <input type='hidden' name='' />
-            <input type='hidden' name='' />
-            <div className={ isChecked === '0' ? `${mainStyle['oil-type']} ${mainStyle['active']}` : mainStyle['oil-type'] } onClick={e => oilTypeCheck(e)}>
+
+            <div className={ isChecked === gasoline ? `${mainStyle['oil-type']} ${mainStyle['active']}` : mainStyle['oil-type'] } onClick={e => oilTypeCheck(e)}>
               <span>휘발유</span>
-              {isChecked === '0' ? <span className={mainStyle['active-arrow']}>&gt;</span> : ''}
-              <input type='radio' name='prodcd' value="B027" defaultChecked='true' hidden/>
+              {isChecked === gasoline ? <span className={mainStyle['active-arrow']}>&gt;</span> : ''}
+              <input type='radio' name='prodcd' value={gasoline} checked={isChecked === gasoline} readOnly hidden/>
             </div>
 
-            <div className={ isChecked === '1' ? `${mainStyle['oil-type']} ${mainStyle['active']}` : mainStyle['oil-type'] } onClick={e => oilTypeCheck(e)}>
+            <div className={ isChecked === diesel ? `${mainStyle['oil-type']} ${mainStyle['active']}` : mainStyle['oil-type'] } onClick={e => oilTypeCheck(e)}>
               <span>경유</span>
-              {isChecked === '1' ? <span className={mainStyle['active-arrow']}>&gt;</span> : ''}
-              <input type='radio' name='prodcd' value="D047" hidden/>
+
+              {isChecked === diesel ? <span className={mainStyle['active-arrow']}>&gt;</span> : ''}
+
+              <input type='radio' name='prodcd' value={diesel} checked={isChecked === diesel} readOnly hidden/>
             </div>
 
-            <div className={ isChecked === '2' ? `${mainStyle['oil-type']} ${mainStyle['active']}` : mainStyle['oil-type'] } onClick={e => oilTypeCheck(e)}>
+            <div className={ isChecked === lpg ? `${mainStyle['oil-type']} ${mainStyle['active']}` : mainStyle['oil-type'] } onClick={e => oilTypeCheck(e)}>
               <span>LPG</span>
-              {isChecked === '2' ? <span className={mainStyle['active-arrow']}>&gt;</span> : ''}
-              <input type='radio' name='prodcd' value='K015' hidden/>
+              {isChecked === lpg ? <span className={mainStyle['active-arrow']}>&gt;</span> : ''}
+              <input type='radio' name='prodcd' value={lpg} checked={isChecked === lpg} readOnly hidden/>
             </div>
-            <button type='submit'>전송</button>
+
           </form>
 
           <div className={mainStyle['banner']}>
@@ -116,95 +181,22 @@ const Main = () => {
 
           <div className={mainStyle['oil-box-api']}>
             <div className={mainStyle['oil-box-map']}>
-            {/* input hidden 으로 ex)서울=01, price 값 넘겨야함 */ }
-              <div className={mainStyle['gyeonggi']}>
-                <span className={mainStyle['title']}>경기</span>
-                <span className={mainStyle['price']}>1983</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['gangwon']}>
-                <span className={mainStyle['title']}>강원</span>
-                <span className={mainStyle['price']}>1999</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['chungbuk']}>
-                <span className={mainStyle['title']}>충북</span>
-                <span className={mainStyle['price']}>1922</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['chungnam']}>
-                <span className={mainStyle['title']}>충남</span>
-                <span className={mainStyle['price']}>2000</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['jeonbuk']}>
-                <span className={mainStyle['title']}>전북</span>
-                <span className={mainStyle['price']}>1880</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['jeonnam']}>
-                <span className={mainStyle['title']}>전남</span>
-                <span className={mainStyle['price']}>1935</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['gyeonbuk']}>
-                <span className={mainStyle['title']}>경북</span>
-                <span className={mainStyle['price']}>1997</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['gyeonnam']}>
-                <span className={mainStyle['title']}>경남</span>
-                <span className={mainStyle['price']}>2002</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['jeju']}>
-                <span className={mainStyle['title']}>제주</span>
-                <span className={mainStyle['price']}>2300</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['seoul']}>
-                <span className={mainStyle['title']}>서울</span>
-                <span className={mainStyle['price']}>2400</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['busan']}>
-                <span className={mainStyle['title']}>부산</span>
-                <span className={mainStyle['price']}>1800</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['daegu']}>
-                <span className={mainStyle['title']}>대구</span>
-                <span className={mainStyle['price']}>1860</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['incheon']}>
-                <span className={mainStyle['title']}>인천</span>
-                <span className={mainStyle['price']}>2150</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['gwangju']}>
-                <span className={mainStyle['title']}>광주</span>
-                <span className={mainStyle['price']}>2150</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['daejeon']}>
-                <span className={mainStyle['title']}>대전</span>
-                <span className={mainStyle['price']}>2150</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['ulsan']}>
-                <span className={mainStyle['title']}>울산</span>
-                <span className={mainStyle['price']}>2150</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
-              <div className={mainStyle['sejong']}>
-                <span className={mainStyle['title']}>세종</span>
-                <span className={mainStyle['price']}>2150</span>
-                <span className={mainStyle['location-dot']}>&bull;</span>
-              </div>
+            { oilPriceList['SidoList'] && oilPriceList['SidoList'].filter(idx => idx.sidonm !== '전국').map( (list) => { 
 
-            </div> {/* //.oil-box-api */}   
-          </div>
+            return (
+
+                <div key={list.sidonm} className={mainStyle[sidoName[list.sidonm]]}>
+                  <span className={mainStyle['title']}>{list.sidonm}</span>
+                  <span className={mainStyle['price']}>{Math.floor(list.price) + ''}</span>
+                  <span className={mainStyle['location-dot']}>&bull;</span>
+                </div>
+
+            )
+
+            })}
+            </div>
+
+          </div>{/* //.oil-box-api */}
           {/* <ul>
           {avgSidoPrice && avgSidoPrice.map((data) => (
           <li key={data.sidocd}>
