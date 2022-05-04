3a01a619508f3a1665ff519185f1742e11919e24
KimCG0 <aksuue824@gmail.com>
2022-05-04 17:28:19 +0900

    형식:상품목록페이지 UI 완성

--- src/main/webapp/reactjs/src/components/goodslist/GoodslistMain.js
+++ src/main/webapp/reactjs/src/components/goodslist/GoodslistMain.js
@@ -6,9 +6,197 @@ import GoodslistMainStyle from './GoodslistMain.module.css';
 const GoodslistMain = () => {
 
     return (
-        <div className={`container text-center ${GoodslistMainStyle['goods-list-layout']}`}>
-            
-        </div>
+        <div className={GoodslistMainStyle['goods-list-wrap']}>
+            <div className={`container text-center ${GoodslistMainStyle['goods-list-layout']}`}>
+                <div className={`container ${GoodslistMainStyle['goods-list-header']}`}>
+                    <h4>상품 목록</h4>
+                    <hr />
+
+                    <div className={GoodslistMainStyle['header-main']}>
+                        <div className={GoodslistMainStyle['header-nav']}>
+                            <ul className='nav'>
+                                <li className='nav-item'>
+                                    <button className={`${GoodslistMainStyle['nav-button']} ${GoodslistMainStyle['active']}`}>전체</button>
+                                </li>
+                                <li className='nav-item'>
+                                    <button className={`${GoodslistMainStyle['nav-button']}`}>실내용품</button>
+                                </li>
+                                <li className='nav-item'>
+                                    <button className={`${GoodslistMainStyle['nav-button']}`}>실외용품</button>
+                                </li>
+                                <li className='nav-item'>
+                                    <button className={`${GoodslistMainStyle['nav-button']}`}>오일</button>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className={GoodslistMainStyle['header-findGoods']}>
+                            <input className={GoodslistMainStyle['findGoods-input']} 
+                                   placeholder='상품명 입력' 
+                            />
+                            <button className={GoodslistMainStyle['findGoods-button']}>상품 검색</button>
+                        </div>
+                    </div>
+                </div> {/* // .goods-list-header */}
+
+                <div className={`container ${GoodslistMainStyle['goods-list-container']}`}>
+
+                    <div className={GoodslistMainStyle['goods-list-main']}>
+                        <div className={GoodslistMainStyle['goods-item']}>
+                            <div className={GoodslistMainStyle['goods-item-img']}>
+                                <img alt='test' src='/images/basket.png' />
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-title']}>
+                                <span>블랙박스</span>
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-price']}>
+                                <div className={GoodslistMainStyle['original-price']}>
+                                    <span>10,000원</span>
+                                    <label>10%</label>
+                                </div>
+                                <div className={GoodslistMainStyle['discount-price']}>
+                                    <span>9,000원</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={GoodslistMainStyle['goods-item']}>
+                            <div className={GoodslistMainStyle['goods-item-img']}>
+                                <img alt='test' src='/images/qna.png' />
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-title']}>
+                                <span>실내용품</span>
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-price']}>
+                                <div className={GoodslistMainStyle['item-price']}>
+                                    <span>50,000원</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                    </div>
+
+                    <div className={GoodslistMainStyle['goods-list-main']}>
+                        <div className={GoodslistMainStyle['goods-item']}>
+                            <div className={GoodslistMainStyle['goods-item-img']}>
+                                <img alt='test' src='/images/basket.png' />
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-title']}>
+                                <span>블랙박스</span>
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-price']}>
+                                <div className={GoodslistMainStyle['original-price']}>
+                                    <span>10,000원</span>
+                                    <label>10%</label>
+                                </div>
+                                <div className={GoodslistMainStyle['discount-price']}>
+                                    <span>9,000원</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={GoodslistMainStyle['goods-item']}>
+                            <div className={GoodslistMainStyle['goods-item-img']}>
+                                <img alt='test' src='/images/qna.png' />
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-title']}>
+                                <span>실내용품</span>
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-price']}>
+                                <div className={GoodslistMainStyle['item-price']}>
+                                    <span>50,000원</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                    </div>
+
+                    <div className={GoodslistMainStyle['goods-list-main']}>
+                        <div className={GoodslistMainStyle['goods-item']}>
+                            <div className={GoodslistMainStyle['goods-item-img']}>
+                                <img alt='test' src='/images/basket.png' />
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-title']}>
+                                <span>블랙박스</span>
+                            </div>
+                            <div className={GoodslistMainStyle['goods-item-price']}>
+                                <div className={GoodslistMainStyle['original-price']}>
+                                    <span>10,000원</span>
+                                    <label>10%</label>
+                                </div>
+                                <div className={GoodslistMainStyle['discount-price']}>
+                                    <span>9,000원</span>
+                                </div>
+                            </div>
+                    </div>
+
+                    <div className={GoodslistMainStyle['goods-item']}>
+                        <div className={GoodslistMainStyle['goods-item-img']}>
+                            <img alt='test' src='/images/qna.png' />
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item-title']}>
+                            <span>실내용품</span>
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item-price']}>
+                            <div className={GoodslistMainStyle['item-price']}>
+                                <span>50,000원</span>
+                        </div>
+                        </div>
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                        <div className={GoodslistMainStyle['goods-item']}>
+
+                        </div>
+                    </div>
+
+                </div> {/* // .goods-list-container */}
+
+                <div className={GoodslistMainStyle['goods-list-footer']}>
+                    <ul className='pagination'>
+                        <li className='page-item'>
+                            <button className={`page-link ${GoodslistMainStyle['page-button']}`}>
+                                &lt;
+                            </button>
+                        </li>
+                        <li className='page-item'>
+                            <button className={`page-link ${GoodslistMainStyle['page-button']} ${GoodslistMainStyle['page-active']}`}>
+                                1
+                            </button>
+                        </li>
+                        <li className='page-item'>
+                            <button className={`page-link ${GoodslistMainStyle['page-button']}`}>2</button>
+                        </li>
+                        <li className='page-item'>
+                            <button className={`page-link ${GoodslistMainStyle['page-button']}`}>3</button>
+                        </li>
+                        <li className='page-item'>
+                            <button className={`page-link ${GoodslistMainStyle['page-button']}`}>&gt;</button>
+                        </li>
+                    </ul>
+                </div> {/* // .goods-list-footer */}
+
+            </div> {/* // .goods-list-layout  */}
+        </div> // .goods-list-wrap
     )
 
 };
