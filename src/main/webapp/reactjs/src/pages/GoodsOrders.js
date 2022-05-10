65874705cc252f6c4c6b114a4bcbbf5ef60ed5ff
KimCG0 <aksuue824@gmail.com>
2022-05-10 17:21:04 +0900

    프로젝트:상품 결제 페이지 생성

---
+++ src/main/webapp/reactjs/src/pages/GoodsOrders.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// import Components
+import Header from 'components/header/Header';
+import GoodsOrdersMain from 'components/goodsorders/GoodsOrdersMain';
+import Footer from 'components/footer/Footer';
+
+const GoodsOrders = () => {
+    
+    return (
+        <div>
+            <Header />
+            <GoodsOrdersMain />
+            <Footer />
+        </div>  
+    );
+};
+
+export default GoodsOrders;
