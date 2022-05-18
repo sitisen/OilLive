1fcf536761ccf84f80ba7317271af3508bbe4cb3
KimCG0 <aksuue824@gmail.com>
2022-05-18 16:19:23 +0900

    프로젝트:결제 테이블 구조 변경으로 인해 VO Field 추가

--- src/main/java/com/oillive/vo/OrdersVO.java
+++ src/main/java/com/oillive/vo/OrdersVO.java
@@ -7,10 +7,13 @@ import lombok.Data;
 @Data
 public class OrdersVO {
 
-	private int orderCode;		// 결제코드
-	private int userCode;		// 회원코드
-	private int goodsCode;		// 차량용품 코드
-	private Date orderDate;		// 결제일
-	private String orderStatus;	// 상태
+	private int orderCode;			// 결제코드
+	private int userCode;			// 회원코드
+	private int goodsCode;			// 차량용품 코드
+	private int orderAmount;		// 판매된 물건 수량
+    private String orderAddress;	// 배송 주소
+    private String orderRequest;	//배송 요청 사항
+	private Date orderDate;			// 결제일
+	private String orderStatus;		// 상태
 	
 }
