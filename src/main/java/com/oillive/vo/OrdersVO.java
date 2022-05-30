0fc644f2fb10898e7e9441c0c1cf67b7421e250b
sitisen <sitisen@naver.com>
2022-05-30 14:36:47 +0900

    형식:주석 정렬

--- src/main/java/com/oillive/vo/OrdersVO.java
+++ src/main/java/com/oillive/vo/OrdersVO.java
@@ -12,9 +12,9 @@ public class OrdersVO {
 	private int goodsCode;			// 차량용품 코드
 	private int orderAmount;		// 판매된 물건 수량
     private String orderAddress;	// 배송 주소
-    private String orderRequest;	//배송 요청 사항
+    private String orderRequest;	// 배송 요청 사항
 	private Date orderDate;			// 결제일
 	private String orderStatus;		// 상태
 	private String goodsName;		// 상품명
-	private int goodsPrice;		// 상품가격
+	private int goodsPrice;			// 상품가격
 }
