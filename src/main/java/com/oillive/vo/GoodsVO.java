b2fba5fa622a265e0f563d2090b0b1eebc054f26
sitisen <sitisen@naver.com>
2022-05-30 16:06:29 +0900

    형식:상품종류 주석 추가

--- src/main/java/com/oillive/vo/GoodsVO.java
+++ src/main/java/com/oillive/vo/GoodsVO.java
@@ -9,7 +9,7 @@ public class GoodsVO {
 
 	private int goodsCode;		 // 차량용품 코드
 	private String goodsName;	 // 상품명
-	private String goodsKind;	 // 상품종류
+	private String goodsKind;	 // 상품종류 ( 실내용품, 세차용품, 엔진오일, 차량 광택제 )
 	private int goodsPrice;		 // 상품가격
 	private int goodsAmount;	 // 상품수량
 	private String goodsContent; // 상품설명
