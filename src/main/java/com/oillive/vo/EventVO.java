776873ea92128c6d1eb5155c3c38b32142a4f654
KimCG0 <aksuue824@gmail.com>
2022-06-06 20:11:03 +0900

    형식:시작일/종료일 타입 Date => String으로 변경

--- src/main/java/com/oillive/vo/EventVO.java
+++ src/main/java/com/oillive/vo/EventVO.java
@@ -1,6 +1,5 @@
 package com.oillive.vo;
 
-import java.util.Date;
 
 import lombok.Data;
 
@@ -10,7 +9,7 @@ public class EventVO {
 	private int eventCode; // 이벤트 코드
 	private String eventName; // 이벤트명
 	private String eventContent; // 이벤트 내용
-	private Date eventStartDate; // 이벤트 시작일
-	private Date eventEndDate; // 이벤트 종료일
+	private String eventStartDate; // 이벤트 시작일
+	private String eventEndDate; // 이벤트 종료일
 	
 }
