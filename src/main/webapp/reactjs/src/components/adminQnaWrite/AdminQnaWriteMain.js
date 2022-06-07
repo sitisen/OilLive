54f50f5439c861a154dac37ebccd154b1654dacc
KimCG0 <aksuue824@gmail.com>
2022-06-07 17:02:15 +0900

    형식:불필요한 useState Import문 제거

--- src/main/webapp/reactjs/src/components/adminQnaWrite/AdminQnaWriteMain.js
+++ src/main/webapp/reactjs/src/components/adminQnaWrite/AdminQnaWriteMain.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import QnAService from 'services/QnAService';
 
